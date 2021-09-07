c43789449ef9327c1a54972dad06764e06afdc8f
MvskokeBoy <59801918+MvskokeBoy@users.noreply.github.com>
2021-09-07 09:57:26 -0700

    Add files via upload
    This incorporates the numpy addition

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import math
+
 """
 Created on Tue Sep  7 09:24:56 2021
 
-@author: deang
 """
 Triton_x_100 = {"cmc":123}
 IGEPal = {"cmc":0.08, "cmcUnit":"miliMoles","molarMass":617}

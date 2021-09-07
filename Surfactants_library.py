f82dddee174b1a8a9fc810fc513a970f51410524
MvskokeBoy <59801918+MvskokeBoy@users.noreply.github.com>
2021-09-07 10:01:31 -0700

    Add files via upload
    Added trition x-100

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -6,7 +6,7 @@ import math
 Created on Tue Sep  7 09:24:56 2021
 
 """
-Triton_x_100 = {"cmc":123}
+Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMoles","molarMass":647}
 IGEPal = {"cmc":0.08, "cmcUnit":"miliMoles","molarMass":617}
 
 print(Triton_x_100["cmc"])

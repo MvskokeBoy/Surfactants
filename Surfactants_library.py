dae3efc9a8ce86eb607d85d740a7c6e2da375b22
MvskokeBoy <59801918+MvskokeBoy@users.noreply.github.com>
2021-09-07 10:22:14 -0700

    Add files via upload
    Added location for funtion

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -6,7 +6,14 @@ import math
 Created on Tue Sep  7 09:24:56 2021
 
 """
-Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMoles","molarMass":647}
+Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMol","molarMass":647}
 IGEPal = {"cmc":0.08, "cmcUnit":"miliMoles","molarMass":617}
 
 print(Triton_x_100["cmc"])
+
+
+"""
+We need to create a function that converts the cmc from milimole
+surfactant to a grams per liter 
+
+"""

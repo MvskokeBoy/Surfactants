002126292ca84ab1d2cb49a22fc286daaf7e5768
fcastro1 <90273074+fcastro1@users.noreply.github.com>
2021-09-07 10:28:36 -0700

    Update Surfactants_library.py

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -6,8 +6,8 @@ import math
 Created on Tue Sep  7 09:24:56 2021
 
 """
-Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMol","molarMass":647}
-IGEPal = {"cmc":0.08,"cmcUnit":"miliMol","molarMass":617}
+Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMolarity","molarMass":647}
+IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617}
 
 print(Triton_x_100["cmc"])
 

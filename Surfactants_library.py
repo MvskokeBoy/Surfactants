303c90e889ce8c874ecc9e7e171e21ceddc9ba6b
fcastro1 <90273074+fcastro1@users.noreply.github.com>
2021-09-08 10:33:32 -0700

    Update Surfactants_library.py
    Added "Polyoxyethylene (10) Tridecyl Ether" to the library.

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -4,10 +4,11 @@ import math
 
 """
 Created on Tue Sep  7 09:24:56 2021
-
 """
+
 Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMolarity","molarMass":647,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sial/x100"}
 IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sigma/i8896"}
+Polyoxyethylene_10_Tridecyl_Ether = {"cmc":0.125,"cmcUnit":"miliMolarity","molarMass":244,"cmcSource":"https://www.researchgate.net/publication/267043941_Cellular_localization_and_detergent_dependent_oligomerization_of_rice_allene_oxide_synthase-1"}
 
 print(Triton_x_100["cmc"])
 

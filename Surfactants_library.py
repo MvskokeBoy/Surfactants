d3efce9018f97ad0dff126b74df360153dc3fd12
bschuld <90355089+bschuld@users.noreply.github.com>
2021-09-08 14:25:36 -0700

    Add files via upload

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -9,8 +9,9 @@ Created on Tue Sep  7 09:24:56 2021
 Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMolarity","molarMass":647,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sial/x100"}
 IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sigma/i8896"}
 Polyoxyethylene_10_Tridecyl_Ether = {"cmc":0.125,"cmcUnit":"miliMolarity","molarMass":244,"cmcSource":"https://www.researchgate.net/publication/267043941_Cellular_localization_and_detergent_dependent_oligomerization_of_rice_allene_oxide_synthase-1"}
+Pluronic_F_68={"cmc":.04,"cmcUnit":"miliMolarity","molarMass":8350,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sigma/p1300" }
 
-print(Triton_x_100["cmc"])
+print(Pluronic_F_68["cmc"])
 
 def cmcConvert(placehold):
     output=placehold["cmc"]*placehold["molarMass"]/1000

773c196d5f6118fd39e8786b85a534ed3dcfde50
bschuld <90355089+bschuld@users.noreply.github.com>
2021-09-08 15:22:13 -0700

    Add files via upload
    added some surfactants tween 80 and Polysorbate_80

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -11,6 +11,9 @@ IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617,"cmcSource":"https
 Polyoxyethylene_10_Tridecyl_Ether = {"cmc":0.125,"cmcUnit":"miliMolarity","molarMass":244,"cmcSource":"https://www.researchgate.net/publication/267043941_Cellular_localization_and_detergent_dependent_oligomerization_of_rice_allene_oxide_synthase-1"}
 Pluronic_F_68={"cmc":.04,"cmcUnit":"miliMolarity","molarMass":8350,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sigma/p1300" }
 Turgiton_NP_10={"cmc":55, "cmcUnit":"ppm","molarMass":682,"cmsSource":"https://www.sigmaaldrich.com/US/en/product/sigma/np10?context=product"}
+Polysorbate_80= {"cmc":0.012,"cmcUnit":"miliMolarity","molarMass":1310,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sial/p1754"}
+Tween_80= {"cmc":0.019,"cmcUnit":"miliMolarity","molarMass":428.62,"cmcSource":"https://www.sciencedirect.com/science/article/pii/S2468023019305486"}
+
 
 
 print(Pluronic_F_68["cmc"])
@@ -18,6 +21,7 @@ print(Pluronic_F_68["cmc"])
 def cmcConvert(placehold):
     if placehold["cmcUnit"]=="ppm":
         placehold["cmc"]=placehold["cmc"]/placehold["molarMass"]
+        #1ppm=1mg/l
     output=placehold["cmc"]*placehold["molarMass"]/1000
     output="{:.5f}".format(output)
     print("Concentration is ",output,"g/L.")

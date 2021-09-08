fee451b2f8c354c0a27192cc516e5a68c7e626b2
fcastro1 <90273074+fcastro1@users.noreply.github.com>
2021-09-08 09:06:23 -0700

    Update Surfactants_library.py
    Added "cmcSource" entry to library, updated for IGEPal and Triton X-100.

--- Surfactants_library.py
+++ Surfactants_library.py
@@ -6,8 +6,8 @@ import math
 Created on Tue Sep  7 09:24:56 2021
 
 """
-Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMolarity","molarMass":647}
-IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617}
+Triton_x_100 = {"cmc": 0.2,"cmcUnit":"miliMolarity","molarMass":647,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sial/x100"}
+IGEPal = {"cmc":0.08,"cmcUnit":"miliMolarity","molarMass":617,"cmcSource":"https://www.sigmaaldrich.com/US/en/product/sigma/i8896"}
 
 print(Triton_x_100["cmc"])
 
